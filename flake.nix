5c978e6bc1c7a9b9aa65b659b326daee3a9af2ec
blezz-tech <markus.jenya04@yandex.ru>
2023-11-19 09:33:11 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,11 +1,50 @@
 {
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv/v0.6.3";
   };
+
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [{
+              packages = with pkgs; [
+                # mariadb
+                nodejs-16_x
+                yarn
+                php81
+              ];
+              # https://devenv.sh/reference/options/
+              # services.mysql = {
+              #   enable = true;
+              #   package = pkgs.mariadb;
+              #   initialDatabases = [{ name = "my-server-db"; }];
+              #   ensureUsers = [
+              #     {
+              #       name = "root";
+              #       password = "";
+              #       ensurePermissions = { "root.*" = "ALL PRIVILEGES"; };
+              #     }
+              #   ];
+              #   settings = {
+              #     # mysql = {
+              #     #   user = "username";
+              #     #   password = "test";
+              #     # };
+              #     mysqld = {
+              #       # "sql_require_primary_key" = "on";
+              #       "bind_address" = "localhost";
+              #     };
+              #   };
+              # };
+            }];
+          };
+        });
+    };
 }
