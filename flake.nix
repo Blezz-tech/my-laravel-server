f8576432f014900a0f04ff94120600050ec95e37
blezz-tech <markus.jenya04@yandex.ru>
2023-11-24 14:49:37 +0300

    update

--- flake.nix
+++ flake.nix
@@ -15,62 +15,29 @@
             inherit inputs pkgs;
             modules = [{
 
-              languages.javascript = {
-                enable = true;
-                package = pkgs.nodejs_20;
-              };
-
               languages.php = {
                 enable = true;
                 version = "8.1";
               };
 
-              # certificates = [
-              #   "example.com"
-              # ];
 
-              # hosts = {
-              #   "example.app" = "127.0.0.1";
+              # services.mysql = {
+              #   enable = true;
+              #   package = pkgs.mariadb;
+              #   initialDatabases = [{ name = "my-laravel-server-db"; }];
+              #   ensureUsers = [
+              #     {
+              #       name = "root";
+              #       password = "";
+              #       ensurePermissions = { "root.*" = "ALL PRIVILEGES"; };
+              #     }
+              #   ];
+              #   settings = {
+              #     mysqld = {
+              #       "bind_address" = "localhost";
+              #     };
+              #   };
               # };
-
-              services.nginx = {
-                enable = true;
-                httpConfig = ''
-                  server {
-                    listen 8080;
-
-                    server_name example.app;
-
-                    charset utf-8;
-
-                    root /home/jenya/git-repos/my-laravel-server;
-
-                    location / {
-                      autoindex on;
-                      autoindex_localtime on;
-                      autoindex_exact_size off;
-                    }
-                  }
-                '';
-              };
-
-              services.mysql = {
-                enable = true;
-                package = pkgs.mariadb;
-                initialDatabases = [{ name = "my-laravel-server-db"; }];
-                ensureUsers = [
-                  {
-                    name = "root";
-                    password = "";
-                    ensurePermissions = { "root.*" = "ALL PRIVILEGES"; };
-                  }
-                ];
-                settings = {
-                  mysqld = {
-                    "bind_address" = "localhost";
-                  };
-                };
-              };
 
               scripts = {
                 EnvClearAll.exec = "rm -rf ./.devenv ./.direnv";
