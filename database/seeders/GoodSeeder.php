2c3bf29d15c36ae5a06590642f4d653f19ccd86b
blezz-tech <markus.jenya04@yandex.ru>
2024-01-16 11:39:11 +0300

    update

--- database/seeders/GoodSeeder.php
+++ database/seeders/GoodSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class GoodSeeder extends Seeder
@@ -12,6 +13,12 @@ class GoodSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('goods')->ignore_user_abort([
+            'title' => Str::random(10),
+            'description' => Str::random(10),
+            'price' => rand(100, 1000),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
