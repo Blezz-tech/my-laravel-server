a300621dfe17cefff5d950d0eb5dcb985103ff54
blezz-tech <markus.jenya04@yandex.ru>
2024-03-12 14:12:24 +0300

    delete old

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,29 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        // $this->call([
-        //     PhotosTableSeeder::class,
-        //     GoodsTableSeeder::class,
-        // ]);
-        $this->call(CatalogPhotoTableSeeder::class);
-        $this->call(CitiesTableSeeder::class);
-        $this->call(CityTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
         $this->call(CountryTableSeeder::class);
         $this->call(CountrylanguageTableSeeder::class);
-        $this->call(FailedJobsTableSeeder::class);
         $this->call(GoodsTableSeeder::class);
-        $this->call(MigrationsTableSeeder::class);
-        $this->call(PasswordResetTokensTableSeeder::class);
-        $this->call(PersonalAccessTokensTableSeeder::class);
         $this->call(PhotosTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
     }
 }
