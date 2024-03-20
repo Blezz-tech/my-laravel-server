bf607e2d9fda2ac09bdd87b50875a99e81e97169
blezz-tech <markus.jenya04@yandex.ru>
2024-03-20 09:56:46 +0300

    update

--- database/seeders/GoodsTableSeeder.php
+++ database/seeders/GoodsTableSeeder.php
@@ -22,20 +22,29 @@ class GoodsTableSeeder extends Seeder
             0 =>
             array(
                 'id' => 1,
-                'title' => 'Мясо',
-                'description' => 'вкусное',
+                'title' => 'мясо',
+                'description' => 'вкусное мяско',
                 'price' => '300.00',
-                'created_at' => '2024-01-16 10:36:57',
-                'updated_at' => '2024-01-16 10:36:57',
+                'created_at' => '2024-01-16 10:05:50',
+                'updated_at' => '2024-01-16 10:05:50',
             ),
             1 =>
             array(
                 'id' => 2,
-                'title' => 'Молоко',
-                'description' => 'свежее молоко натуральное',
+                'title' => 'сыр',
+                'description' => 'сливочный сырок',
+                'price' => '150.00',
+                'created_at' => '2024-01-16 10:05:50',
+                'updated_at' => '2024-01-16 10:05:50',
+            ),
+            2 =>
+            array(
+                'id' => 3,
+                'title' => 'картошка',
+                'description' => 'самая мягкая картошечка',
                 'price' => '80.00',
-                'created_at' => '2024-01-16 10:36:57',
-                'updated_at' => '2024-01-16 10:36:57',
+                'created_at' => '2024-01-16 10:05:50',
+                'updated_at' => '2024-01-16 10:05:50',
             ),
         ));
     }
